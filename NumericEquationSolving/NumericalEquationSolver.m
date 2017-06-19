7ae2e62ae07f1f9530ea60d51863d3508a3cd310
Benardi <jose.nunes@ccc.ufcg.edu.br>
2017-06-19 00:36:39 -0300

    Stop condition now takes into account relative error

--- NumericEquationSolving/NumericalEquationSolver.m
+++ NumericEquationSolving/NumericalEquationSolver.m
@@ -23,7 +23,7 @@ classdef NumericalEquationSolver
             end
         end
         
-        function self = testInterval(self, a, b)
+        function r = testInterval(self, a, b)
             if self.testIntervalContinuity(a,b, self.Equation) == 0      
                 error('Equation contains descontinuity at interval')
                 
@@ -32,9 +32,6 @@ classdef NumericalEquationSolver
 
             elseif self.testHypothesis(a,b) == 0
                 error('First Derivative doesn''t preserve signal at interval')
-            else
-                self.a = a;
-                self.b = b;
             end
         end
         
@@ -64,9 +61,10 @@ classdef NumericalEquationSolver
                r = obj;  
         end  
         
-        function r = testCandidate(obj, entry)
+        function r = testCandidate(obj, entry, previous)
+            relativeError = abs((entry - previous)/entry); 
             estimation = abs(vpa(subs(obj.Equation,symvar(obj.Equation,1),entry)));
-            if estimation <= obj.Precision || estimation == 0
+            if estimation <= obj.Precision || estimation == 0 || relativeError < obj.Precision
                 r = 1;
             else
                 r = 0;
--- NumericEquationSolving/bissection.m
+++ NumericEquationSolving/bissection.m
@@ -1,19 +1,19 @@
 syms x a b
 format long
 n = NumericalEquationSolver(x^2 - 4, 0.0000000000000000000000001);
-n.testInterval(0,100);
 tries = 5000; 
-a = 1;
-b = 2;
+a = -100;
+b = 0;
+n.testInterval(a,b);
 
 root = 'Not found';
 run = 0;
-
+x0 = 0;
 while run < tries
     xk = (a + b)/2;
     root = xk;
     
-    if n.testCandidate(xk) == 1
+    if n.testCandidate(xk, x0) == 1
         break;
     end    
         
@@ -24,7 +24,7 @@ while run < tries
        a = xk;
     end
     run = run + 1;
-                       
+    x0 = xk;                   
 end
 disp(run);
 disp(root);
