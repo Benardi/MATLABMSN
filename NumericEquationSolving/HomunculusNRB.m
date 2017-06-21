26ae40b062f45f5e73475deb050613600bc43131
Benardi <jose.nunes@ccc.ufcg.edu.br>
2017-06-21 19:38:21 -0300

    Homunculus now follows Hussein Altaee Hoomod ideas

--- NumericEquationSolving/HomunculusNRB.m
+++ NumericEquationSolving/HomunculusNRB.m
@@ -1,47 +1,40 @@
 syms x a b
 format long
-n = NumericalEquationSolver(exp(x), 0.01e-90);
+n = NumericalEquationSolver(x^4 + 3*x^3 -15*x^2 -2^x + 9, 0.1e-10);
 tries = 5000; 
-a = -1;
-b = 2;
-x0 = 0;
-r = n.testIntervalforNewton(a, b, x0, 1);
+a = -8;
+b = -4;
 
-switchVar = 0;
-if r == 0
-    switchVar = 1;
+%r = n.testIntervalBissection(a,b,0);
+
+if r == 1
     return
 end
 
 root = 'Not found';
-run = 0;
-previous = x0;
-while run < tries
-    if switchVar == 0 % calls Newton's Method
-        fxk = vpa(subs(n.Equation,symvar(n.Equation,1),previous));
-        f1xk = vpa(subs(diff(n.Equation),symvar(n.Equation,1),previous));
-        xk = previous - (fxk/f1xk);
-        root = xk;
-        
-        % If Newton's Method generates divergence in xk switchs to Bissection
-        if n.testFurtherDivergence(previous, xk) == 1
-            switchVar = 1;
-            previous = 0;
-        end
-        
-    else % calls Bissection Method
-        xk = (a + b)/2;        
-        root = xk;
 
-        if n.testTheorem1(a, xk) == 1
-            b = xk;
-               
-        else
-            a = xk;
-        end
-        previous = xk;
-        
+for i = 1:2
+    % calls Bissection Method
+    xk = (a + b)/2;        
+    root = xk;
+
+    if n.testTheorem1(a, xk) == 1
+        b = xk;       
+    else
+        a = xk;
     end
+end
+
+previous = xk;
+
+%r = n.testIntervalforNewton(a, b, previous, 1);
+
+run = 0;
+while run < (tries -2) % calls Newton's Method
+    fxk = vpa(subs(n.Equation,symvar(n.Equation,1),previous));
+    f1xk = vpa(subs(diff(n.Equation),symvar(n.Equation,1),previous));
+    xk = previous - (fxk/f1xk);
+    root = xk;   
     
     if n.testExactMatch(xk) == 1
         break;
