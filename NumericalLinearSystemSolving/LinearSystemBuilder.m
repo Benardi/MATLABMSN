c44bc06d93dd3c7d22d282f49e7a40f729b6215a
Benardi <jose.nunes@ccc.ufcg.edu.br>
2017-07-15 00:06:03 -0300

    Creation of class to suport solving of Linear Systems

---
+++ NumericalLinearSystemSolving/LinearSystemBuilder.m
@@ -0,0 +1,33 @@
+classdef LinearSystemBuilder
+    %LINEARSYSTEMBUILDER Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        EquationsMatrix % the matrix of multiple equations
+    end
+    
+    methods
+        function obj = LinearSystemBuilder(matrix)
+            obj.EquationsMatrix = matrix;
+            
+        end
+        
+        function r = extractPolynomDegree(self, polyn)
+               r = length(sym2poly(polyn))- 1;
+        end
+        
+        function r = highestDegree(self)
+            max = 0;
+            for equ = self.EquationsMatrix
+                temp = self.extractPolynomDegree(equ);
+                if( temp > max)
+                    max = temp;
+                end
+            end
+            r = max;
+        end
+       
+    end
+    
+end
+
