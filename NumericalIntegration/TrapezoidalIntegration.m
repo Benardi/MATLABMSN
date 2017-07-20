9ef72d6d8a9f2b437a667ef3dacab4b133daabb1
Benardi <jose.nunes@ccc.ufcg.edu.br>
2017-07-20 11:04:21 -0300

    Logic relayed to class. Implementation of composite Trapezoidal Rule

--- NumericalIntegration/TrapezoidalIntegration.m
+++ NumericalIntegration/TrapezoidalIntegration.m
@@ -17,6 +17,30 @@ classdef TrapezoidalIntegration
             T = subs(self.Equation, symvar(self.Equation, 1), a);
             r = vpa((h/2) * (t + T));
         end
+        
+        function r = createsSeriesOfIntervals(self, a,b,numbOfPoints)
+            r = [];
+            r = [a r];
+            for i=1:(numbOfPoints-2)
+                intSect = (b * i)/(numbOfPoints-1);
+                r = [r intSect];
+            end
+            r = [r b];
+
+        end
+        
+        function r = compositeTrapezoidalRule(self,a,b, numbOfPoints)
+            interval = self.createsSeriesOfIntervals(a,b,numbOfPoints);
+            r = 0;
+            for i=1:(length(interval) -1)
+                a = interval(i);
+                b = interval(i+1);
+                localRes = self.simpleTrapezoidalRule(a,b);
+                r = r + localRes;
+            end
+        end
+        
+        
     end
     
 end
--- NumericalIntegration/TrapezoidalRule.m
+++ NumericalIntegration/TrapezoidalRule.m
@@ -1,23 +1,13 @@
 syms x a b;
+
 tr = TrapezoidalIntegration((1 + x^2)^(-1/2)); 
 f(x) = (1 + x^2)^(-1/2);
 tries = 5000; 
 a = 0;
 b = 4;
 
-h = b - a;
-t = f(b);
-T = f(a);
-result = vpa((h/2) * (t + T));
-disp(result);
+interval = tr.createsSeriesOfIntervals(a,b,9);
 disp(tr.simpleTrapezoidalRule(a,b));
-
-interval = [];
-interval = [a interval];
-points = 10;
-for i=1:(points-2)
-    intSect = (b * i)/(points-1);
-    interval = [interval intSect];
-end
-interval = [interval b];
-disp(interval);
+disp(tr.compositeTrapezoidalRule(a,b,2));
+disp(tr.compositeTrapezoidalRule(a,b,3));
+disp(tr.compositeTrapezoidalRule(a,b,9));
